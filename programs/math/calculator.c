e568f2d0007e44c2a315a8f8f6fc76b04d6f2461
Anatoliy6463 <120381885+Anatoliy6463@users.noreply.github.com>
2022-12-12 17:58:51 +0300

    Калькулятор
    Калькулятор для TolyaDOS

---
+++ programs/math/calculator.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(void)
+{
+int A;
+int B;
+int C;
+char znak;
+scanf (%d, %A);
+scanf (%d, %B);
+scanf (%c, %znak);
+if (znak == '+') C = A + B;
+if (znak == '-') C = A + B;
+if (znak == '*') C = A + B;
+if (znak == '/') C = A + B;
+if (znak == '^') 
+{
+int a2;
+for (int i = 0; i < A; i++)
+{
+a2*=a;
+}
+printf (a2);
+return (0);
+}
+printf (C);
+return (0);
+}

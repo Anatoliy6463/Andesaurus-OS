113e8ddb6b669819c8957440aa30bd78ddca7c85
Anatoliy6463 <120381885+Anatoliy6463@users.noreply.github.com>
2022-12-12 21:22:59 +0300

    Косинус

---
+++ programs/math/cosinus.cpp
@@ -0,0 +1,11 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+int main()
+{
+  int number;
+  cin >> number;
+  int cosinus = cos(number);
+  cout <<"cos= "<<cosinus<<endl;
+  return 0;
+}

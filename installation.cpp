7f0c1eb593dff0d0324a874a5f1c0ebf06fac443
Anatoliy6463 <120381885+Anatoliy6463@users.noreply.github.com>
2022-12-27 15:03:55 +0300

    Принятие лицензионного соглашения

---
+++ installation.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main()
+{
+  cout <<"Чтобы установить TolyaDOS 0.0.6-publicalpha.1 вы должны прочитать лицензию GNU Public License 3.0 \n";
+  string soglasie;
+  cout <<"Вы принимаете условия лицензии? \n";
+  cin >> soglasie;
+  if (soglasie == "Да" || soglasie == "Oui" || soglasie == "Yes")
+  {
+    cout <<"Доступ разрешён\n";
+  }
+  else
+  {
+    cout <<"Доступ запрещён\n";
+  }
+  return 0;
+}

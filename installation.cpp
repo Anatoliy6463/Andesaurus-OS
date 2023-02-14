fa7d79b45dc3f73906fd54253ea3eaf143d73f0a
Anatoliy6463 <120381885+Anatoliy6463@users.noreply.github.com>
2023-02-14 13:57:58 +0300

    Загрузка

--- installation.cpp
+++ installation.cpp
@@ -3,13 +3,16 @@
 using namespace std;
 int main()
 {
-  cout <<"Чтобы установить TolyaDOS 0.0.6-publicalpha.1 вы должны прочитать лицензию GNU Public License 3.0 \n";
+  cout <<"Чтобы установить TolyaDOS 1.0.5-2 вы должны прочитать лицензию GNU Public License 3.0 \n";
   string soglasie;
   cout <<"Вы принимаете условия лицензии? \n";
   cin >> soglasie;
   if (soglasie == "Да" || soglasie == "Yes")
   {
     cout <<"Доступ разрешён\n";
+    FILE *fp;
+    fp = fopen("DOS", "r+b");
+    cout <<fp<<endl;
   }
   else
   {

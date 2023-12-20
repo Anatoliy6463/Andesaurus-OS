ce2656b950a6c3c7dab542995b1e8bd7c81260ad
Anatoliy6463 <120381885+Anatoliy6463@users.noreply.github.com>
2023-12-20 14:04:45 +0300

    Create keys.h

---
+++ keys.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*Library for controls in T-GameC
+Copyright (C) 2023  Andesaurus-Sesto*/
+void persikinteract(int X, int Y, int persiki, int hunger, int in)
+{
+    if (X % 9 == 0 && X != 0 || Y % 9 == 0 && Y != 0) ++persiki;  
+    if (in == 'p') printf("Количество персиков:%d\n", persiki);
+    if (in == 'e' && persiki > 0) 
+    {
+	puts("Вы съели персик!\n");
+	if (hunger <= 20) hunger = 0;
+        if (hunger > 20) hunger = hunger - 20;
+	persiki++;
+    }
+    if (in == 'e' && persiki == 0)
+      puts("У вас нет персиков, найдите или купите их.\n");
+}

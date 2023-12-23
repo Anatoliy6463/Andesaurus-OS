217680d72f21c2d417b73059a83b43a01b3e94d8
Anatoliy6463 <120381885+Anatoliy6463@users.noreply.github.com>
2023-12-23 17:25:08 +0300

    Update games.h

--- games.h
+++ games.h
@@ -164,3 +164,21 @@ void ugadaika_extreme()
         }
     }
 }
+int biletik()
+{
+    int nomer = 0;
+    puts("Введите шестизначное число\n");
+    scanf("%d", &nomer);
+    int a=0, b=0, c=0, d=0, e=0, f=0;
+    a = nomer % 10;
+    b = (nomer / 10) % 10;
+    c = (nomer / 100) % 10;
+    d = (nomer / 1000) % 10;
+    e = (nomer / 10000) % 10;
+    f = (nomer / 100000) % 10;
+    if (a + b + c == e + d + f)
+        cout << "Билет счастливый! \n";
+    else
+        cout << "Билет несчастливый. \n";
+    return 0;
+}

f1bf0654121efe8b41e54fb8cc6fa6d5af5a9b20
Anatoliy6463 <120381885+Anatoliy6463@users.noreply.github.com>
2023-12-26 19:09:50 +0300

    Update events.h

--- extra/events.h
+++ extra/events.h
@@ -1,9 +1,10 @@
+#include <stdbool.h>
 #include <stdio.h>
 /*Library for events in T-GameC
 Copyright (C) 2023 Andesaurus-Sesto*/
-void shop(int money, int ta, int persiki)
+void shop(int money, int ta, int persiki, bool armor, bool sword)
 {
-	puts("Добро пожаловать в магазин! Что бы вы хотели купить? Телепортатор = 1, персики = 2");
+	puts("Добро пожаловать в магазин! Что бы вы хотели купить? Телепортатор = 1, персики = 2, броня = 3, меч = 4");
 	int shopin;
 	scanf("%d", &shopin);
 	if (shopin == 1)
@@ -17,6 +18,16 @@ void shop(int money, int ta, int persiki)
 		scanf("%d", &shopin);
 		money = money - 1 * shopin;
         persiki = persiki * shopin;
+	}
+	if (shopin == 3)
+	{
+		armor = true;
+		money -= 20;
+	}
+	if (shopin == 4)
+	{
+		sword = true;
+		money -= 15;
 	}
 }
 void check(int ta, int result) 
@@ -53,4 +64,21 @@ int die()
 {
   puts("GAME OVER!\007\n");
   return (0);
+}
+void dragon(int hp, int inp, int money, int dragonhp, bool armor, bool sword)
+{
+	puts("Здесь живёт дракон!\n");
+	inp = getchar();
+	if (armor == false) hp -= 20;
+	if (armor == true) hp -= 5;
+	if (inp == 'Z')
+	{
+		if (sword == true) dragonhp -= 5;
+		if (sword == false) dragonhp -= 2;
+	}
+	if (dragonhp < 0)
+	{
+		puts("Вы победили дракона!\n\007\007\007");
+		money+=1000;
+	}
 }

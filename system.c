a7678211b41bfb0ba3833053172556f1b813aee4
Anatoliy6463 <120381885+Anatoliy6463@users.noreply.github.com>
2023-12-31 20:35:35 +0300

    Update system.c

--- system.c
+++ system.c
@@ -60,55 +60,73 @@ int main()
             {
                 puts("Отмена...\n");
                 in[0] = '\0';
+                getchar();
             }
+            getchar();
         }
         if (strcmp(in, "ugadaika") == 0 && inst[UGADAIKA] == 0)
         {
             puts("Программа \"ugadaika\" не найдена, но её можно установить с помощью команды pkg install ugadaika");
+            getchar();
         }
         else if (strcmp(in, "pkg install random_number") == 0 && inst[RANNUM] == 0)
         {
             puts("Установка \"random_number\"...\n");
             inst[RANNUM] = 1;
             puts("Установка завершена!\n");
+            getchar();
         }
         else if (strcmp(in, "pkg install max_min") == 0 && inst[MAXMIN] == 0)
         {
             puts("Установка \"max_min\"...\n");
             inst[MAXMIN] = 1;
             puts("Установка завершена!\n");
+            getchar();
         }
         else if (strcmp(in, "pkg install square_gen_and_print") == 0 && inst[CUBE] == 0)
         {
             puts("Установка \"square_gen_and_print\"...\n");
             inst[CUBE] = 1;
             puts("Установка завершена!\n");
+            getchar();
         }
         else if (strcmp(in, "pkg install stxt") == 0 && inst[STXT] == 0)
         {
             puts("Установка \"stxt\"...\n");
             inst[STXT] = 2;
             puts("Установка завершена! Теперь нужно установить stxt-filecreate\n");
+            getchar();
         }
         else if (strcmp(in, "pkg install ugadaika") == 0 && inst[UGADAIKA] == 0)
         {
             puts("Установка \"ugadaika\"...\n");
             inst[UGADAIKA] = 1;
             puts("Установка завершена!\n");
+            getchar();
+        }
+        else if (strcmp(in, "pkg install t-gamec") == 0 && inst[GAME] == 0)
+        {
+            puts("Установка \"t-gamec\"...\n");
+            inst[GAME] = 1;
+            puts("Установка завершена!\n");
+            getchar();
         }
         else if (strcmp(in, "pkg install stxt-filecreate") == 0 && inst[STXT] == 2)
         {
             puts("Установка \"stxt-filecreate\"...\n");
             inst[STXT] = 1;
             puts("Установка завершена!\n");
+            getchar();
         }
         else if (strcmp(in, "info") == 0)
         {
             puts("SestoOS  Copyright (C) 2023  Anatoliy6463\nThis program comes with ABSOLUTELY NO WARRANTY; for details, type \"show w\"\nThis is free software, and you are welcome to redistribute it under certain conditions;\n");
+            getchar();
         }
         else if (strcmp(in, "show w") == 0)
         {
             puts("SestoOS comes with ABSOLUTELY NO WARRANTY, to the extent permitted by applicable law.\n");
+            getchar();
         }
         else if (strcmp(in, "exit") == 0)
         {
@@ -118,27 +136,37 @@ int main()
         else if (strcmp(in, "wait") == 0)
         {
             waitmode();
+            getchar();
         }
         else if (strcmp(in, "calc") == 0 && inst[CALC] == 1)
         {
             calculator();
+            getchar();
         }
         else if (strcmp(in, "calc") == 0 && inst[CALC] == 0)
         {
             puts("Программа \"calc\" не найдена, но её можно установить с помощью команды pkg install calc");
+            getchar();
         }
         else if (strcmp(in, "game") == 0 && inst[GAME] == 1)
         {
             game();
+            getchar();
         }
         else if (strcmp(in, "game") == 0 && inst[GAME] == 0)
         {
             puts("Программа \"game\" не найдена, но её можно установить с помощью команды pkg install t-gamec");
+            getchar();
         }
-        else if(strcmp(in, "007") == 0) printf("%c", '\007');
+        else if(strcmp(in, "007") == 0) 
+        {
+            printf("%c", '\007');
+            getchar();
+        }
         else if(strcmp(in, "Когда выйдет Электроманьяк?") == 0)
         {
             puts("В январе 2024\n");
+            getchar();
         }
         else if(strcmp(in, "random_number") == 0 && inst[RANNUM] == 1)
         {
@@ -168,10 +196,12 @@ int main()
                 printf("%d", random_number[i]);
             }
             puts("\n");
+            getchar();
         }
         else if (strcmp(in, "random_number") == 0 && inst[RANNUM] == 0)
         {
             puts("Программа \"random_number\" не найдена, но её можно установить с помощью команды pkg install random_number");
+            getchar();
         }
         else if (strcmp(in, "max_min") == 0 && inst[MAXMIN] == 1) 
         {
@@ -180,32 +210,65 @@ int main()
             scanf("%d", &a);
             int n[a];
             max_and_min(a, n);
+            getchar();
+        }
+        else if (strcmp(in, "file_read") == 0)
+        {
+            puts("Введите название файла:\t");
+            char filenam[128] = {0};
+            fgets(filenam, 127, stdin);
+            if ((strlen(filenam) > 0) && (filenam[strlen (in) - 1] == '\n'))
+                filenam[strlen (in) - 1] = '\0';
+            FILE *f;
+            if ((f = fopen(filenam, "r")) == NULL)
+            {
+                puts("Не удалось найти файл\n\007Создать файл? [y/N]");
+                int vb;
+                vb = getchar();
+                if (vb == 'y' || vb == 'Y')
+                    f = fopen(filenam, "w");
+                getchar();
+            }
+            else {
+                f = fopen(filenam, "r");
+                char buffer[2048] = {0};
+                fread(buffer, 2048, 1, f);
+                fclose(f);
+                printf("Состав:\n%s\n", buffer);
+                getchar();
+            }
         }
         else if (strcmp(in, "max_min") == 0 && inst[MAXMIN] == 0)
         {
             puts("Программа \"max_min\" не найдена, но её можно установить с помощью команды pkg install max_min");
+            getchar();
         }
         else if(strcmp(in, "square_gen") == 0 && inst[CUBE] == 1)
         {
             int n;
             square_gen_and_print(n);
+            getchar();
         }
         else if (strcmp(in, "square_gen") == 0 && inst[CUBE] == 0)
         {
             puts("Программа \"square_gen\" не найдена, но её можно установить с помощью команды pkg install square_gen_and_print");
+            getchar();
         }
         else if(strcmp(in, "help") == 0)
         {
             puts("ugadaika – игра «Угадайка»\nchange_usr – сменить пользователя\ninfo – информация о системе\nexit – выход\ncalc – калькулятор\ngame – T-GameC\n007 – звук предупреждения системы\nrandom_number – генератор случайных чисел\nsquare_gen – генератор квадрата\ntime – текущее время\nmax_min – нахождение максимального и минимального числа\n");
+            getchar();
         }
         else if (strcmp(in, "time") == 0 && inst[TIME] == 1)
         {
             time(&t);
             printf("%s\n", ctime(&t));
+            getchar();
         }
         else if (strcmp(in, "time") == 0 && inst[TIME] == 0)
         {
             puts("Программа \"time\" не найдена, но её можно установить с помощью команды pkg install time");
+            getchar();
         }
         else if (strcmp(in, "stxt") == 0 && inst[STXT] == 1)
         {
@@ -217,10 +280,12 @@ int main()
         else if (strcmp(in, "stxt") == 0 && inst[STXT] == 0)
         {
             puts("Программа \"stxt\" не найдена, но её можно установить с помощью команды pkg install stxt, а затем pkg install stxt-filecreate");
+            getchar();
         }
         else if (strcmp(in, "stxt") == 0 && inst[STXT] == 2)
         {
             puts("Программа \"stxt\" найдена, но зависимость \"stxt-filecreate\" не установлена, её можно установить с помощью команды pkg install stxt, а затем pkg install stxt-filecreate");
+            getchar();
         }
         else if (strcmp(in, "kill system") == 0)
         {
@@ -229,6 +294,7 @@ int main()
         }
         else {
             printf("%s, команда не найдена, для получения списка команд, введите \"help\"\n", in);
+            getchar();
         }
     }
 }

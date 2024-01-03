bbde2f87b9e98fc125a872ef168f120c9937514a
Anatoliy6463 <120381885+Anatoliy6463@users.noreply.github.com>
2024-01-03 18:54:00 +0300

    Update and rename keyfrases.h to keyf.h

--- keyf.h
+++ keyf.h
@@ -2,11 +2,12 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 /*
 *    Command list for Andesaurus-OS.
 *    Copyright (C) 2023  AndesaurusSoft
 */
-int waitmode()
+void waitmode()
 {
     while(1)
     {
@@ -21,4 +22,16 @@ void killsys()
     puts("Kernel panic!!!\n");
     sleep(rand()%10 + 1);
     return;
+}
+void deleten(char* in)
+{
+    if ((strlen(in) > 0) && (in[strlen (in) - 1] == '\n'))
+        in[strlen (in) - 1] = '\0';
+}
+void clear(char* in)
+{
+    for (int i = 0; i < sizeof(in); i++)
+    {
+        in[i] = '\0';
+    }
 }

dcef433328040e7db2bdfe1dcc359edb0b7bcb47
Anatoliy6463 <120381885+Anatoliy6463@users.noreply.github.com>
2024-01-04 22:51:02 +0300

    Create terminal.h

---
+++ terminal.h
@@ -0,0 +1,102 @@
+#include <string.h>
+#include "base.h"
+#include "apps.h"
+#include <stdio.h>
+#include "games.h"
+int terminal()
+{
+	char input[256];
+	while (strcmp(input, "exit") != 0)
+	{
+		puts("user@minian:\t");
+		fgets(input, 255, stdin);
+		deleten(input);
+		if (strcmp(input, "help") == 0)
+		{
+			puts("exit - выход из Minian\ncreate_file - создать файл\nread_file - прочитать файл\ngame - игра \"T-GameC\"\n");
+      			clear(input); // очистка буфера
+		}
+		else if (strcmp(input, "create_file") == 0)
+		{
+			puts("Введите название файла:\t");
+			char filenam[256];
+			fgets(filenam, 255, stdin);
+			deleten(filenam);
+			if (fopen(filenam, "r") != NULL)
+			{
+				puts("ФАЙЛ УЖЕ СУЩЕСТВУЕТ!!!\007\n");
+				clear(input);
+			}
+			else
+			{
+				FILE *bin = fopen(filenam, "w");
+				fclose(bin);
+				clear(input);
+			}
+		}
+		else if (strcmp(input, "info") == 0)
+		{
+			puts("Это версия Andesaurus-OS, которая использует Minian в качестве эмулятора терминала вместо Andesaurus Shell\n");
+			clear(input);
+		}
+		else if (strcmp(input, "bubble_sort") == 0)
+		{
+			puts("Введите количество чисел, а затем введите сами числа:\t");
+			int a = 0;
+			scanf("%d", &a);
+			int n[a];
+			for (int i = 0; i < a; i++)
+				n[i] = 0;
+			bubble_sort(a, n);
+			puts("Сортировка завершена! Запуск печати...\n\n\n\n\n\n");
+			for (int i = 0; i < a; i++)
+			{
+				printf("%d\n", n[i]);
+			}
+		}
+		else if (strcmp(input, "read_file") == 0)
+		{
+			puts("Введите название файла:\t");
+			char filenam[256];
+			fgets(filenam, 255, stdin);
+			deleten(filenam);
+			FILE *f = fopen(filenam, "r");
+			if (f == NULL)
+			{
+				puts("ФАЙЛА НЕ СУЩЕСТВУЕТ, ПОЖАЛУЙСТА, СОЗДАЙТЕ ЕГО С ПОМОЩЬЮ КОМАНДЫ create_file\n\007");
+				clear(input);
+			}
+			else
+			{
+				FILE *f = fopen(filenam, "r");
+				char ch;
+				do {
+        				ch = fgetc(f);
+        				printf("%c", ch);
+    				} while (ch != EOF);
+				puts("\n");
+				clear(input);
+			}
+		}
+		else if (strcmp(input, "\0") == 0)
+		{
+			clear(input);
+		}
+		else if (strcmp(input, "game") == 0)
+		{
+			puts("Загрузка...\n");
+			game();
+		}
+		else if (strcmp(input, "exit") == 0)
+		{
+			puts("Завершение...\n");
+			clear(input);
+			int returncode = 0;
+			return returncode;
+		}
+		else 
+		{
+			printf("%s - команда не найдена, для получения списка команд, введите help\n\007", input);
+		}
+	}
+}

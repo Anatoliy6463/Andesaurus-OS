c36f830c509cc9b4be57a68251d1ec685cf50c20
Anatoliy6463 <120381885+Anatoliy6463@users.noreply.github.com>
2024-01-04 22:36:42 +0300

    Update base.h

--- base.h
+++ base.h
@@ -5,3 +5,22 @@ void deleten(char* in)
     if ((strlen(in) > 0) && (in[strlen (in) - 1] == '\n'))
         in[strlen (in) - 1] = '\0';
 }
+void login(int result)
+{
+	char usrpass[16] = {0};
+	FILE *f = fopen("rootpass.txt", "r");
+	char validpass[16]; 
+	fgets(validpass, 15, f);
+	fgets(usrpass, 15, stdin);
+	deleten(validpass);
+	if (strcmp(usrpass, validpass) == 0)
+	{
+		result = 0;
+		return;
+	}
+	else {
+		result = 1;
+		puts("\007\007\007\t\t\tПАРОЛЬ НЕПРАВИЛЬНЫЙ!!!\n");
+		return;
+	}
+}
